de8cb27871dda84bb8e4c608b70f3f61fd976757
Christian Maré <chris@mare.org.uk>
2020-05-06 08:10:31 +0100

    Convert to hooks

--- src/AnyHotPass.js
+++ src/AnyHotPass.js
@@ -1,71 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import generate from 'anyhotpass-lib';
+import generatePass from 'anyhotpass-lib';
 
-class AnyHotPass extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { master: '', domain: '', password: '', hidden: true };
-    this.masterChanged = this.masterChanged.bind(this);
-    this.domainChanged = this.domainChanged.bind(this);
-    this.handleGenerate = this.handleGenerate.bind(this);
-    this.handleToggle = this.handleToggle.bind(this);
-    this.handleFocus = (event) => event.target.select();
-  }
+function AnyHotPass() {
+  const [master, setMaster] = useState('');
+  const [domain, setDomain] = useState('');
+  const [password, setPassword] = useState('');
+  const masterRef = useRef(null);
+  const passwordRef = useRef(null);
 
-  render() {
-    return (
-      <>
-        <Modal show="true" backdrop="false">
-          <Modal.Header>
-            <Modal.Title>AHP</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group controlId="formPassword">
-                <Form.Control type="password" placeholder="Master Password" required onChange={this.masterChanged} />
-              </Form.Group>
-              <Form.Group controlId="formDomain">
-                <Form.Control type="url" placeholder="example.com" required onChange={this.domainChanged} />
-              </Form.Group>
-              <Button variant="secondary" type="submit" block onClick={this.handleGenerate} class="mr-1" >Generate</Button>
-              <Form.Group controlId="formDomain" className="mt-2">
-                <Form.Control type="text" readOnly onFocus={this.handleFocus} value={this.state.password} />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </>
-    )
-  }
+  useEffect(() => {
+    masterRef.current.focus();
+  });
 
-  masterChanged(e) {
-          this.setState({ master: e.target.value });
-  }
-
-  domainChanged(e) {
-          this.setState({ domain: e.target.value });
-  }
-
-  handleGenerate(e) {
-          e.preventDefault();
-    if (this.state.master.length === 0 || this.state.domain.length === 0) {
+  const generate = (e) => {
+    e.preventDefault();
+    if (master.length === 0 || domain.length === 0) {
       return;
     }
-    const password = generate(this.state.master, this.state.domain, 18);
-    this.setState(state => ({
-          password: password
-    }));
-  }
+    const newPassword = generatePass(master, domain, 18);
+    setPassword(newPassword);
+    passwordRef.current.focus();
+  };
 
-  handleToggle(e) {
-          e.preventDefault();
-    this.setState(state => ({
-          hidden: !this.state.hidden
-    }));
-  }
+  return (
+    <>
+      <Modal show="true" backdrop="false">
+        <Modal.Header>
+          <Modal.Title>AHP</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formPassword">
+              <Form.Control type="password" ref={masterRef} placeholder="Master Password" required onChange={(e) => setMaster(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formDomain">
+              <Form.Control type="url" placeholder="example.com" required onChange={setDomain((e) => e.target.value)} />
+            </Form.Group>
+            <Button variant="secondary" type="submit" block onClick={(e) => generate(e)} class="mr-1" >Generate</Button>
+            <Form.Group controlId="formDomain" className="mt-2">
+              <Form.Control type="text" ref={passwordRef} readOnly onFocus={(e) => e.target.select()} value={password} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
 }
 
 export default AnyHotPass;
